1321303a477906b43f6f3865cdec350e1747564f
kibebr <itsmekibe@gmail.com>
2021-05-28 14:45:15 +0100

    a

--- server/index.js
+++ server/index.js
@@ -1,4 +1,15 @@
 import WebSocket from 'ws'
+import express from 'express'
+
+const PORT = 8081
+
+const server = express()
+
+server.use((_, res) => res.send('test'))
+
+server.listen(PORT, () => {
+  console.log('listening on port 8080')
+})
 
 const wss = new WebSocket.Server({ port: 8080 })
 
